0d618a73e697f6340aa4778a7a3ea70c0d0e1170
dalina15 <61637677+dalina15@users.noreply.github.com>
2021-05-05 01:29:58 -0300

    Initial commit

---
+++ app/src/main/java/com/troyanos/airelibre/Login.java
@@ -0,0 +1,56 @@
+package com.troyanos.airelibre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText usuario, contrasena;
+    Button btn_ingresar;
+    BDHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usuario = (EditText) findViewById(R.id.usuario1);
+        contrasena = (EditText) findViewById(R.id.contrasena3);
+        btn_ingresar = (Button) findViewById(R.id.btn_ingresar1);
+        DB = new BDHelper(this);
+
+        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = usuario.getText().toString();
+                String passcode = contrasena.getText().toString();
+
+                if (user.equals("") || passcode.equals(""))
+                    Toast.makeText(Login.this, getResources().getString(R.string.datos_faltantes), Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean verificarusuariocontrasena = DB.verificarusuariocontrasena(user, passcode);
+                    if (verificarusuariocontrasena == true) {
+                        Toast.makeText(Login.this, getResources().getString(R.string.bienvenida), Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplication(), MapsActivity.class);
+
+
+                        // **** Acá cambiar el mapeo de loading a maps!  ****
+
+
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Login.this, getResources().getString(R.string.datos_incorrectos), Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+            }
+        });
+    }}
---
+++ app/src/main/java/com/troyanos/airelibre/MainActivity.java
@@ -0,0 +1,90 @@
+package com.troyanos.airelibre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usuario, contrasena, contrasena2;
+    Button registro, ingresar, recordar_mas_tarde;
+    BDHelper DB;
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progressbar);
+        textView = findViewById(R.id.textView);
+
+
+        usuario = (EditText) findViewById(R.id.usuario);
+        contrasena = (EditText) findViewById(R.id.contrasena);
+        contrasena2 = (EditText) findViewById(R.id.contrasena2);
+        registro = (Button) findViewById(R.id.btn_registro);
+        ingresar = (Button) findViewById(R.id.btn_ingresar);
+        recordar_mas_tarde = (Button) findViewById(R.id.btn_recordar_mas_tarde);
+        DB = new BDHelper(this);
+
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = usuario.getText().toString();
+                String passcode = contrasena.getText().toString();
+                String passcode2 = contrasena2.getText().toString();
+
+                if (user.equals("") || passcode.equals("") || passcode2.equals(""))
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.datos_faltantes), Toast.LENGTH_SHORT).show();
+                else {
+                    if (passcode.equals(passcode2)) {
+                        Boolean verificarusuario = DB.verificarusuario((user));
+                        if (verificarusuario == false) {
+                            Boolean insert = DB.insertarData(user, passcode);
+                            if (insert == true) {
+                                Toast.makeText(MainActivity.this, getResources().getString(R.string.datos_correctos), Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(MainActivity.this, getResources().getString(R.string.comprobar_datos), Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, getResources().getString(R.string.usuario_existente), Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, getResources().getString(R.string.validar_contrasena), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+
+            }
+        });
+        recordar_mas_tarde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }}
+
